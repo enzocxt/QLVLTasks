88a8625ac852712f12a8021cd3ae17c2fe4d829b
enzocxt <enzocxt@gmail.com>
2018-02-04 18:35:53 +0100

    modify: object oriented

--- corpus_convert/split_files.py
+++ corpus_convert/split_files.py
@@ -3,180 +3,230 @@ import sys
 import shutil
 import codecs
 
+from tqdm import tqdm
 
-def parse(filename, output_dir):
-    meta_dict = file_metadata()
-    skip_set = {'t_of_memory', 'me_out', 'art_hoot(time_out)'}
-    with codecs.open(filename, 'r', encoding='latin1') as inf:
-        context = inf.read()
-        sentences = context.split('</pre>')
-        sentences = [(s + '</pre>').strip() for s in sentences]
-        cur_file_id = ''
-        cur_file = []
-        for s in sentences:
-            file_id = ''
-            for line in s.split('\n'):
-                if line.strip().startswith('<file-id>'):
-                    first = '<file-id>'
-                    end = '</file-id>'
-                    startidx = line.index(first) + len(first)
-                    endidx = line.index(end, startidx)
-                    file_id = line[startidx:endidx]
-                    break
-
-            if file_id == '' or file_id in skip_set:
-                continue
-            if cur_file_id == '':   # first time
-                cur_file_id = file_id
-                cur_file.append(s)
-            elif cur_file_id != file_id:  # meets a new file and output the previous one
-                cur_fname = get_output_fname_SoNaR(cur_file_id, output_dir, meta_dict)
-                context = '\n'.join(cur_file)
-                write_file(cur_fname, context)
-                cur_file_id = file_id
-                cur_file = [s]
-            else:
-                cur_file.append(s)
+import logging
+import functools
+import traceback
 
 
-def file_metadata():
-    metaBE_fname = "/home/enzocxt/Projects/QLVL/other_tasks/corpus_convert/WR-P-P-G_newspapers.lectinfo.bel.txt"
-    metaNL_fname = "/home/enzocxt/Projects/QLVL/other_tasks/corpus_convert/WR-P-P-G_newspapers.lectinfo.ned.txt"
+def trace(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        indent = ' ' * len(traceback.extract_stack())
+        try:
+            logging.debug('{0}entering {1}'.format(indent, fn.__name__))
+            return fn(*args, **kwargs)
+        except:
+            logging.debug('{0}exception in {1}'.format(indent, fn.__name__))
+            raise
+        else:
+            logging.debug('{0}leaving {1}'.format(indent, fn.__name__))
 
-    meta_dict = dict()
-    with codecs.open(metaBE_fname, 'r', encoding='latin1') as inf:
-        _header = inf.readline()
-        metadata = [line.strip().split('"')[1::2] for line in inf.readlines()]
-        for data in metadata:
-            file_id = data[0][:-9]
-            date = ''.join(data[6].split('-'))
-            meta_dict[file_id] = (data[5], date)
-
-    with codecs.open(metaNL_fname, 'r', encoding='latin1') as inf:
-        _header = inf.readline()
-        metadata = [line.strip().split('"')[1::2] for line in inf.readlines()]
-        for data in metadata:
-            if data[1].startswith('WR-P'):
-                newspaper = 'unknown'
-            else:
-                newspaper = data[1].split(': ')[1].replace(' ', '_').lower()
-            file_id = data[0][:-9]
-            date = ''.join(data[6].split('-'))
-            meta_dict[file_id] = (newspaper, date)
-    return meta_dict
+    return wrapper
 
 
-def get_output_fname_SoNaR(file_id, output_dir, meta_dict):
-    ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad'}
-    bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws'}
+class Parser(object):
 
-    if file_id not in meta_dict:
-        print "file id not in meta info: {}".format(file_id)
-    krant, datum = meta_dict.get(file_id, ('unknown', '00000000'))
-    jaar = datum[:4]
-    if krant in ned_set:
-        nation = 'Netherlands'
-    elif krant in bel_set:
-        nation = 'Belgium'
-    else:
-        nation = 'Other'
-    name = file_id.split('-')[-1]
-    filename = "{output}/{nation}/{year}/{news}/{prefix}_{name}.conll".format(
-               output=output_dir, nation=nation, year=jaar, news=krant,
-               prefix=krant+datum, name=name)
+    @classmethod
+    @trace
+    def parse(cls, filename, output_dir, meta_dict):
+        skip_set = {'t_of_memory', 'me_out', 'art_hoot(time_out)'}
+        with codecs.open(filename, 'r', encoding='latin1') as inf:
+            context = inf.read()
+            sentences = context.split('</pre>')
+            sentences = [(s + '</pre>').strip() for s in sentences]
+            cur_file_id = ''
+            cur_file = []
+            for s in sentences:
+                file_id = ''
+                for line in s.split('\n'):
+                    if line.strip().startswith('<file-id>'):
+                        first = '<file-id>'
+                        end = '</file-id>'
+                        startidx = line.index(first) + len(first)
+                        endidx = line.index(end, startidx)
+                        file_id = line[startidx:endidx]
+                        break
 
-    # recursively create directories if they do not exist
-    folders = filename.split('/')
-    for i in range(3, len(folders)):
-        path = '/'.join(folders[:i])
-        if not os.path.exists(path):
-            try:
-                os.makedirs(path)
-            except OSError as exc:
-                if exc.errno != errno.EEXIST:
-                    raise
-
-    return filename
-
-
-def get_output_fname_LeNC(file_id, output_dir):
-    ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad', 'parool'}
-    bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws', 'het_nieuwsblad', 'belang_van_limburg',
-               'de_tijd'}
-    meta_dict = {
-        'ad': 'algemeen_dagblad',
-        'nrc': 'nrc_handelsblad',
-        'trouw': 'trouw',
-        'parool': 'parool',
-        'volkskrant': 'volkskrant',
-        'DS': 'de_standaard',
-        'BL': 'belang_van_limburg',
-        'LN': 'het_laatste_nieuws',
-        'DT': 'de_tijd',
-        'NB': 'het_nieuwsblad',
-        'DM': 'de_morgen',
-    }
-
-    # 'NB_20051229_01.alpino.xml'
-    name = file_id.split('.')[0]
-    krant, datum, id = name.split('_')
-    krant = meta_dict.get(krant, 'unknown')
-    jaar = datum[:4]
-
-    # 'nrc20040408.alpino.xml'
-
-    if krant in ned_set:
-        nation = 'Netherlands'
-    elif krant in bel_set:
-        nation = 'Belgium'
-    else:
-        nation = 'Other'
-    filename = "{outdir}/{nation}/{year}/{news}/{krant}_{datum}_{id}.conll".format(
-        outdir=output_dir, nation=nation, year=jaar, news=krant,
-        krant=krant, datum=datum, id=id)
-
-    return filename
+                if file_id == '' or file_id in skip_set:
+                    continue
+                if cur_file_id == '':   # first time
+                    cur_file_id = file_id
+                    cur_file.append(s)
+                elif cur_file_id != file_id:  # meets a new file and output the previous one
+                    cur_fname = FilenameGetter.get_output_fname_SoNaR(cur_file_id, output_dir, meta_dict)
+                    context = '\n'.join(cur_file)
+                    write_file(cur_fname, context)
+                    cur_file_id = file_id
+                    cur_file = [s]
+                else:
+                    cur_file.append(s)
 
 
-def get_output_fname_TwNC(file_id, output_dir):
-    ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad', 'parool'}
-    bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws', 'het_nieuwsblad', 'belang_van_limburg', 'de_tijd'}
-    meta_dict = {
-        'ad': 'algemeen_dagblad',
-        'nrc': 'nrc_handelsblad',
-        'trouw' : 'trouw',
-        'parool' : 'parool',
-        'volkskrant' : 'volkskrant',
-        'DS': 'de_standaard',
-        'BL': 'belang_van_limburg',
-        'LN': 'het_laatste_nieuws',
-        'DT': 'de_tijd',
-        'NB': 'het_nieuwsblad',
-        'DM': 'de_morgen',
-    }
+class MetaData(object):
+    def __init__(self, meta_file_list=None):
+        metaBE_fname = "/home/enzocxt/Projects/QLVL/other_tasks/corpus_convert/WR-P-P-G_newspapers.lectinfo.bel.txt"
+        metaNL_fname = "/home/enzocxt/Projects/QLVL/other_tasks/corpus_convert/WR-P-P-G_newspapers.lectinfo.ned.txt"
+        if meta_file_list is None or len(meta_file_list) == 0:
+            self.meta_file_list = [metaBE_fname, metaNL_fname]
+        else:
+            self.meta_file_list = meta_file_list
 
-    # 'nrc20040408.alpino.xml'
-    name = file_id.split('.')[0]
-    idx = 0
-    for i, c in enumerate(name):
-        if c.isdigit():
-            idx = i
-            break
-    krant, datum = name[:idx], name[idx:]
-    krant = meta_dict.get(krant, 'unknown')
-    jaar = datum[:4]
+        self.meta_dict = dict()
 
-    if krant in ned_set:
-        nation = 'Netherlands'
-    elif krant in bel_set:
-        nation = 'Belgium'
-    else:
-        nation = 'Other'
-    filename = "{outdir}/{nation}/{year}/{news}/{krant}_{datum}.conll".format(
-        outdir=output_dir, nation=nation, year=jaar, news=krant,
-        krant=krant, datum=datum)
+    def read_metadata(self):
+        metaBE_fname = self.meta_file_list[0]
+        metaNL_fname = self.meta_file_list[1]
 
-    return filename
+        metaBE_dict = self.read_meta_BE(metaBE_fname)
+        self.meta_dict.update(metaBE_dict)
+        metaNL_dict = self.read_meta_NL(metaNL_fname)
+        self.meta_dict.update(metaNL_dict)
+
+        return self.meta_dict
+
+    def read_meta_BE(self, filename):
+        meta_dict = dict()
+        with codecs.open(filename, 'r', encoding='latin1') as inf:
+            _header = inf.readline()
+            metadata = [line.strip().split('"')[1::2] for line in inf.readlines()]
+            for data in metadata:
+                file_id = data[0][:-9]
+                date = ''.join(data[6].split('-'))
+                meta_dict[file_id] = (data[5], date)
+        return meta_dict
+
+    def read_meta_NL(self, filename):
+        meta_dict = dict()
+        with codecs.open(filename, 'r', encoding='latin1') as inf:
+            _header = inf.readline()
+            metadata = [line.strip().split('"')[1::2] for line in inf.readlines()]
+            for data in metadata:
+                if data[1].startswith('WR-P'):
+                    newspaper = 'unknown'
+                else:
+                    newspaper = data[1].split(': ')[1].replace(' ', '_').lower()
+                file_id = data[0][:-9]
+                date = ''.join(data[6].split('-'))
+                meta_dict[file_id] = (newspaper, date)
+        return meta_dict
+
+
+class FilenameGetter(object):
+
+    @classmethod
+    def get_output_fname_SoNaR(cls, file_id, output_dir, meta_dict):
+        ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad'}
+        bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws'}
+
+        if file_id not in meta_dict:
+            print "file id not in meta info: {}".format(file_id)
+        krant, datum = meta_dict.get(file_id, ('unknown', '00000000'))
+        jaar = datum[:4]
+        if krant in ned_set:
+            nation = 'Netherlands'
+        elif krant in bel_set:
+            nation = 'Belgium'
+        else:
+            nation = 'Other'
+        name = file_id.split('-')[-1]
+        filename = "{output}/{nation}/{year}/{news}/{prefix}_{name}.conll".format(
+                   output=output_dir, nation=nation, year=jaar, news=krant,
+                   prefix=krant+datum, name=name)
+
+        # recursively create directories if they do not exist
+        folders = filename.split('/')
+        for i in range(3, len(folders)):
+            path = '/'.join(folders[:i])
+            if not os.path.exists(path):
+                try:
+                    os.makedirs(path)
+                except OSError as exc:
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+        return filename
+
+    @classmethod
+    def get_output_fname_LeNC(cls, file_id, output_dir):
+        ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad', 'parool'}
+        bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws', 'het_nieuwsblad', 'belang_van_limburg',
+                   'de_tijd'}
+        meta_dict = {
+            'ad': 'algemeen_dagblad',
+            'nrc': 'nrc_handelsblad',
+            'trouw': 'trouw',
+            'parool': 'parool',
+            'volkskrant': 'volkskrant',
+            'DS': 'de_standaard',
+            'BL': 'belang_van_limburg',
+            'LN': 'het_laatste_nieuws',
+            'DT': 'de_tijd',
+            'NB': 'het_nieuwsblad',
+            'DM': 'de_morgen',
+        }
+
+        # 'NB_20051229_01.alpino.xml'
+        name = file_id.split('.')[0]
+        krant, datum, id = name.split('_')
+        krant = meta_dict.get(krant, 'unknown')
+        jaar = datum[:4]
+
+        # 'nrc20040408.alpino.xml'
+
+        if krant in ned_set:
+            nation = 'Netherlands'
+        elif krant in bel_set:
+            nation = 'Belgium'
+        else:
+            nation = 'Other'
+        filename = "{outdir}/{nation}/{year}/{news}/{krant}_{datum}_{id}.conll".format(
+            outdir=output_dir, nation=nation, year=jaar, news=krant,
+            krant=krant, datum=datum, id=id)
+
+        return filename
+
+    @classmethod
+    def get_output_fname_TwNC(cls, file_id, output_dir):
+        ned_set = {'nrc_handelsblad', 'trouw', 'volkskrant', 'unknown', 'algemeen_dagblad', 'parool'}
+        bel_set = {'de_standaard', 'dm_magazine', 'de_morgen', 'het_laatste_nieuws', 'het_nieuwsblad', 'belang_van_limburg', 'de_tijd'}
+        meta_dict = {
+            'ad': 'algemeen_dagblad',
+            'nrc': 'nrc_handelsblad',
+            'trouw' : 'trouw',
+            'parool' : 'parool',
+            'volkskrant' : 'volkskrant',
+            'DS': 'de_standaard',
+            'BL': 'belang_van_limburg',
+            'LN': 'het_laatste_nieuws',
+            'DT': 'de_tijd',
+            'NB': 'het_nieuwsblad',
+            'DM': 'de_morgen',
+        }
+
+        # 'nrc20040408.alpino.xml'
+        name = file_id.split('.')[0]
+        idx = 0
+        for i, c in enumerate(name):
+            if c.isdigit():
+                idx = i
+                break
+        krant, datum = name[:idx], name[idx:]
+        krant = meta_dict.get(krant, 'unknown')
+        jaar = datum[:4]
+
+        if krant in ned_set:
+            nation = 'Netherlands'
+        elif krant in bel_set:
+            nation = 'Belgium'
+        else:
+            nation = 'Other'
+        filename = "{outdir}/{nation}/{year}/{news}/{krant}_{datum}.conll".format(
+            outdir=output_dir, nation=nation, year=jaar, news=krant,
+            krant=krant, datum=datum)
+
+        return filename
 
 
 def write_file(filename, context):
@@ -216,43 +266,36 @@ def copy_to_tmp(filename, tmpdir):
         return False
 
 
-def convert(input_dir, tmpDIR_dir, output_dir, tmpOUT_dir, command):
-    files = os.listdir(input_dir)
-    i = 0
+def convert(input_dir, tmpDIR_dir, output_dir, tmpOUT_dir, meta_dict, command, pg_leave=True):
+    depth = len(traceback.extract_stack()) - 2
+    indent = '  ' * depth
+    files = tqdm(os.listdir(input_dir), unit='file', desc='{}corpus'.format(indent), leave=pg_leave)
     for f in files:
         fname = '{}/{}'.format(input_dir, f)
         res = check_fname(fname)
         if res < 0:
             continue
         elif res == 0:
-            convert(fname, tmpDIR_dir, output_dir, tmpOUT_dir, command)
+            convert(fname, tmpDIR_dir, output_dir, tmpOUT_dir, meta_dict, command, pg_leave=False)
             continue
 
         input_fname = copy_to_tmp(fname, tmpDIR_dir)
-        if not input_fname:
-            raise Exception("Processing file with suffix other than xml, gzip")
-        i += 1
-
         tmp_in_fname = input_fname.split('/')[-1]
         tmp_out_fname = '.'.join(tmp_in_fname.split('.')[:-1] + ['conllu'])
         output_fname = '{}/{}'.format(tmpOUT_dir, tmp_out_fname)
 
         cmd = "{} DIR={} -o:{}".format(command, tmpDIR_dir, output_fname)
-        print("excuting system command for {} file:\n  {}".format(i, cmd))
+        # print("excuting system command for {} file:\n  {}".format(i, cmd))
         os.system(cmd)
-        print("done")
+        # print("done")
 
         # split intermediate output into files
-        parse(output_fname, output_dir)
+        Parser.parse(output_fname, output_dir, meta_dict)
 
         # remove input file in tmpDIR_dir
         os.system("rm {}".format(input_fname))
         # remove output file in tmpOUT_dir
         os.system("rm {}".format(output_fname))
-
-        if (i + 1) % 10 == 0:
-            # print("converted {} files".format(i + 1))
-            print ".",
 
 
 def main_SoNaR(input_dir, output_dir, command):
@@ -267,7 +310,7 @@ def main_SoNaR(input_dir, output_dir, command):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # create a tmp directory for xQuery (under output directory)
+    # create a tmp directory for xQuery's input (under output directory)
     tmpDIR_dir = '{}/tmpDIR'.format(output_dir)
     if os.path.exists(tmpDIR_dir):
         shutil.rmtree(tmpDIR_dir)
@@ -278,8 +321,12 @@ def main_SoNaR(input_dir, output_dir, command):
         shutil.rmtree(tmpOUT_dir)
     os.makedirs(tmpOUT_dir)
 
+    # read meta data
+    md = MetaData()
+    meta_dict = md.read_metadata()
+
     # recursively convert corpus files in input directory
-    convert(input_dir, tmpDIR_dir, output_dir, tmpOUT_dir, command)
+    convert(input_dir, tmpDIR_dir, output_dir, tmpOUT_dir, meta_dict, command)
 
     # clean tmp folders
     os.system("rm -r {}".format(tmpDIR_dir))
@@ -327,9 +374,9 @@ def main_LeNCTwNC(corpus_name, input_dir, output_dir, command, dtd_fname=None):
         in_filename = input_fname.split('/')[-1]
         # get output filename based on input filename
         if corpus_name == 'LeNC':
-            output_fname = get_output_fname_LeNC(in_filename, output_dir)
+            output_fname = FilenameGetter.get_output_fname_LeNC(in_filename, output_dir)
         elif corpus_name == 'TwNC':
-            output_fname = get_output_fname_TwNC(in_filename, output_dir)
+            output_fname = FilenameGetter.get_output_fname_TwNC(in_filename, output_dir)
         else:
             raise Exception("Incorrect corpus name: {}".format(corpus_name))
 
